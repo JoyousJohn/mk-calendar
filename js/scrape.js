2a66a9b1b6e6e51034f51c5022b23de014613dd9
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2020-10-13 20:34:19 -0400

    Split names

--- js/scrape.js
+++ js/scrape.js
@@ -1,15 +1,37 @@
 function knollsSelected() {
     $(".schoolOptions").fadeOut()
-    document.getElementById("knollsCalendar").style.visibility = "visible"
+    document.getElementById("knollsCalendar").style.visibility = "visible" //Hides MK/MH buttons
     scrapeKnolls()
 }
 
 async function scrapeKnolls() {
-    response = await axios.get("https://cors-anywhere.herokuapp.com/calendar.google.com/calendar/u/0/htmlembed?src=mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com&ctz=America/New_York")
-    //console.log(response.data)
-    var tempSummaries = []
-    for (i = 0; i < $(response.data).find(".event-summary").length; i++) {
-        tempSummaries.push($(response.data).find(".event-summary").eq(i).text())
+    try {
+        response = await axios.get("https://cors-anywhere.herokuapp.com/calendar.google.com/calendar/u/0/htmlembed?src=mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com")
+    } catch (err) { console.log(err)
     }
-    console.log(tempSummaries)
+    //console.log(response.data)
+    var eventName = []
+    var eventWhere = []
+
+    var tempEvents = []
+    for (i = 0; i < $(response.data).find(".event-summary").length; i++) { //Loops through events
+        tempEvents.push($(response.data).find(".event-summary").eq(i).text()) //Adds events to array
+
+        if(tempEvents[i].includes("VIRTUAL - ")) {
+            eventWhere[i] = "Virtual"
+            splitEvent = tempEvents[i].split("VIRTUAL - ")
+            eventName[i] = splitEvent[1]
+        } else if(tempEvents[i].includes(" (Virtual)")) {
+            eventWhere[i] = "Virtual"
+            splitEvent = tempEvents[i].split(" (Virtual)")
+            eventName[i] = splitEvent[0]
+        } else {
+            eventName[i] = tempEvents[i]
+        }
+    }
+    console.log(tempEvents);
+    for(i = 1; i < 16; i++) {
+        $("#event" + [i]).text("Name: " + eventName[i]);
+    }
+
 }
