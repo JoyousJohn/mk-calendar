a71118bfb2e140a07f096fef9aa4c353d41481a4
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2020-10-16 22:57:39 -0400

    Many improvements with sorting events
    Along with a month and year getter and listening even properties in the temp. labels

--- js/knolls.js
+++ js/knolls.js
@@ -1,47 +1,65 @@
 var eventName = []
-var eventStatus = []
+var eventStatus = [] //Possible: null, Virtual, Canceled
 var eventRoom = []
+var eventStart = []
+var eventDate = []
+
+let month
+let year
 
 function getKnolls(data) { //Gets event info
     var defaultEventSummaries = [] //Just to see array in console
-    for (i = 0; i < $(data).find(".event-summary").length; i++) { //Loops through events
+    for (i = 0; i < $(data).find(".item-content event-singleday, .event-summary").length; i++) { //Loops through events
         eventName.push($(data).find(".event-summary").eq(i).text()) //Adds events to array
         defaultEventSummaries.push($(data).find(".event-summary").eq(i).text()) //Adds events to array
 
+        //console.log(eventName[i])
         testVirtual(eventName[i], i) //Checks if virtual
         testCanceled(eventName[i], i) //Checks if canceled
         testRooms(eventName[i], i) //Looks for room numbers
+        removeAcronyms(eventName[i], i) //Replaces acronyms like "mtg" with "meeting"
     }
     console.log(defaultEventSummaries)
     $("#eventQuantity").text(eventName.length + " events found:") //Displays events found (temp)
     tempSetLabels() //Sets (temp) labels to event info
+
+    //console.log(data);
+    var periodRange = $(data).filter('.period-range').text().split(" ") //Spent an hour trying to figure this out: https://stackoverflow.com/questions/400197/extracting-ajax-return-data-in-jquery
+    month = periodRange[0]
+    year = periodRange[1]
+    $("#calendarTime").text(month + " " + year)
+
+    for (i = 0; i < $(data).find(".event-time").length; i++) { //Loops through events
+        eventStart.push($(data).find(".event-time").eq(i).text()) //Adds event start time to array
+    }
+    console.log(eventStart)
+    //console.log(periodRange)
 }
 
-function testVirtual(event, eventNum) { //Sees if virtual
-    if(event.toLowerCase().includes("virtual")) { //.includes is case sensitive for some reason
-        if(event.includes("VIRTUAL - ")) { //At beginning of event-summary
-            splitEvent = event.split("VIRTUAL - ")
-            eventName[eventNum] = splitEvent[1]
-
-        } else if(event.includes(", (Virtual)")) { //At end of event-summary //Unique for "NHS Meeting,"
-            splitEvent = event.split(", (Virtual)")
-            eventName[eventNum] = splitEvent[0]
-
-        } else if(event.includes(" (Virtual)")) { //At end of event-summary
-            splitEvent = event.split(" (Virtual)")
-            eventName[eventNum] = splitEvent[0]
-
-        } else if(event.includes("VIRTUAL (Zoom) - ")) { //At beginning of event-summary
-
-            splitEvent = event.split("VIRTUAL (Zoom) - ")
-            eventName[eventNum] = splitEvent[1]
-        }
+function testVirtual(event, eventNum) { //Checks if virtual
+    var virtualMessagesFront = ["VIRTUAL (Zoom) - ", "VIRTUAL - "]
+    var virtualMessagesEnd = [" (Zoom)", ", (Virtual)", " (Virtual)"]
+    if(event.toLowerCase().includes("virtual")||event.toLowerCase().includes("zoom")) { //Ugh I had the or statement in the wrong place for like 20 minutes!
+        //console.log(eventNum);
         eventStatus[eventNum] = "Virtual"
-    }
-    else if(event.includes(" (Zoom)")) { //At end of event-summary //Took me a while to realize this doesn't have "virtual" in it!
-        //console.log(eventNum)
-        splitEvent = event.split(" (Zoom)")
-        eventName[eventNum] = splitEvent[0]
+        var skip
+        for(var i = 0; i < virtualMessagesFront.length; i++) {
+            if(event.includes(virtualMessagesFront[i])) { //If the event-summary includes a virtual/zoon keyword in front
+                splitEvent = event.split(virtualMessagesFront[i])
+                eventName[eventNum] = splitEvent[1]
+                skip = true //Skips attempting checking other keywords at end (below)
+                break
+            }
+        }
+        if(skip != true) { //If not told to skip
+            for(var i = 0; i < virtualMessagesEnd.length; i++) { //Loops each keyword
+                if(event.includes(virtualMessagesEnd[i])) { //If the event-summary includes a virtual/zoon keyword at end
+                    splitEvent = event.split(virtualMessagesEnd[i])
+                    eventName[eventNum] = splitEvent[0]
+                    break
+                }
+            }
+        }
     }
 }
 
@@ -58,16 +76,10 @@ function testCanceled(event, eventNum) { //Sees if canceled
 
 function testRooms(event, eventNum) { //Checks event-summary for room numbers
     //console.log(eventNum)
-    if(event.includes("- B")) { //If in a B wing room //Not sure why I have to use toLowerCase here. I'll figure it out later!
-        for(a = 60; a > 0; a--) { //Future proofing even though there are only a few B rooms in the calendar list! //Using i really messed stuff up... //Backwards since doens't consider one digit rooms
-            //console.log(eventNum + " " + a)
-            if(event.includes("- B" + a)) { // Ex. "- B59" @ end of event-summary
-                splitEvent = event.split("- B" + a)
-                eventName[eventNum] = splitEvent[0]
-                eventRoom[eventNum] = "B" + a //Sets room number
-                a = 0 //Stops loop, can't place this above. Note: doesn't terminate what's below before the closing bracket of the if statement.
-            }
-        }
+    if(event.includes("- B")) { //If in a B wing room //I was using sooo many loops here originally, ouch!
+        eventRoom[eventNum] = event.substring(event.indexOf("- B") + 2, event.indexOf("- B") + 5) //Sets room number
+        splitEvent = event.split("- B")
+        eventName[eventNum] = splitEvent[0]
     //Two Rooms
     } else if(event.includes(", B")) { //Don't have to do toLowerCase here?...
         //Two rooms, i.g. "B55/B57"
@@ -80,14 +92,15 @@ function testRooms(event, eventNum) { //Checks event-summary for room numbers
             var rooms
             if(event.includes("-B")) { //Just realized my first large loop with testRooms is so unnecessary!
                 rooms = event.substring(event.indexOf(", B") + 2, event.lastIndexOf(", "))
-                rooms = rooms.replace("-", ",")
+                //rooms = rooms.replace("-", ",") What was I thinking? I can't do this!
             } if(event.includes("School Counseling Office")) { //Should I really do if statements like this? :thinking:
                 rooms += ",School Counseling Office"
             }
             eventRoom[eventNum] = rooms
             splitEvent = event.split(", B")
-            eventName[eventNum] = splitEvent[0] //Sets new name        }
-
+            eventName[eventNum] = splitEvent[0] //Sets new name
+        }
+    //C wing
     } else if(event.toLowerCase().includes(", c")) { //If in a C wing room
         for(a = 3; a > 0; a--) { //Trying all 3 C wing rooms (Are there more? Idk, the highest on the calendar so far was 3!)
             if(event.toLowerCase().includes(", c" + a)) { // Ex. "- B59" @ end of event-summary
@@ -101,11 +114,29 @@ function testRooms(event, eventNum) { //Checks event-summary for room numbers
         splitEvent = event.split(", Library Classroom") //At end of event-summary
         eventName[eventNum] = splitEvent[0]
         eventRoom[eventNum] = "Library" //Sets room number
+    } else if(event.includes(", Aud")) { //If in library (I think?)
+        splitEvent = event.split(", Aud") //At end of event-summary
+        eventName[eventNum] = splitEvent[0]
+        eventRoom[eventNum] = "Auditorium" //Sets room number
+    } else if(event.includes(", Dance Studio")) { //If in library (I think?)
+        splitEvent = event.split(", Dance Studio") //At end of event-summary
+        eventName[eventNum] = splitEvent[0]
+        eventRoom[eventNum] = "Dance Studio" //Sets room number
     }
 }
 
+function removeAcronyms (event, eventNum) {
+    eventName[eventNum] = event.replace("Mtg", "Meeting")
+}
+
 function tempSetLabels() { //Sets website labels with event info
-    for(i = 1; i < 60; i++) {
-        $("#event" + [i]).text("Name: " + eventName[i])
+    for(i = 0; i < 61; i++) {
+        var extra = ""
+        if(eventStatus[i] != null) {
+            extra = " - Status: " + eventStatus[i]
+        } if(eventRoom[i] != null) {
+            extra += " - Room(s): " + eventRoom[i]
+        }
+        $("#event" + [i + 1]).text("Name: " + eventName[i] + " " + extra)
     }
 }
