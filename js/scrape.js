88bdbc1e4199699ff51127939b7c72467965e80a
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2020-12-29 23:54:55 -0500

    Improvements
    Improved animations, shifted from useless complicated HTTP searching to Google's Calendar API, and added date retrieval

--- js/scrape.js
+++ js/scrape.js
@@ -1,15 +1,34 @@
 function knollsSelected() {
-    $(".schoolOptions").fadeOut()
-    document.getElementById("knollsCalendar").style.visibility = "visible" //Hides MK/MH buttons
-    scrapeKnolls()
+    $(".schoolOptions").fadeOut() //Hides MK/MH buttons
+    setTimeout(function(){
+        $("#knollsDiv").fadeIn() //Shows temp labels
+    }, 300);
+    setDates()
+    scrapeKnolls() //Begins entire process
 }
 
 async function scrapeKnolls() {
+    //let id = "mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com"
+    //let key = "AIzaSyA3Fshq5WSPcvNe8zQTXnbCe6VUArfo13w"
+    let site = "https://www.googleapis.com/calendar/v3/calendars/mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com/events?key=AIzaSyA3Fshq5WSPcvNe8zQTXnbCe6VUArfo13w&timeMin=2021-01-01T00:00:00-00:00"
     try {
-        response = await axios.get("https://cors-anywhere.herokuapp.com/calendar.google.com/calendar/u/0/htmlembed?src=mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com")
-        //response = await axios.get("https://api.allorigins.win/get?url=calendar.google.com/calendar/u/0/htmlembed?src=mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com")
-        //response = await axios.get("http://corsproxy.nodester.com/?src=pastebin.com/raw/sHP8fzfk")
-    } catch (err) { console.log(err)
+        response = await axios.get(site)
+    } catch (error) { console.log(error) }
+    console.log(response.data)
+}
+
+function setDates() {
+    var d = new Date() //New object
+    //var date = d.getDate()
+    //var month = d.getMonth() + 1 //+1 since JS counts January as 0
+    //console.log("date " + date + " " + month)
+    var firstDay = new Date(d.getFullYear(), d.getMonth(), 1)
+    firstDay = firstDay.getDay() + 1 //First weekday day of month. i.e. 5 = Friday and is 1st day of month
+    var lastDate = new Date(d.getFullYear(), d.getMonth()+1, 0)
+    lastDate = lastDate.getDate() //Gets final date
+    var dateCount = 1
+    for(var a = firstDay; dateCount <= lastDate; a++) {
+        $("#slot" + a).text(dateCount)
+        dateCount++
     }
-    getKnolls(response.data)
 }
