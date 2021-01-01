2c3a4519a892d266039fcd1ac5b61c81b497c1a3
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2021-01-01 17:53:08 -0500

    Improvements & simplifications

--- js/scrape.js
+++ js/scrape.js
@@ -1,44 +1,43 @@
+var date = new Date() //Date object of now. NEED global to span more than one month either direction
+
 function knollsSelected() {
     $(".schoolOptions").fadeOut() //Hides MK/MH buttons
     setTimeout(function(){
         $("#knollsDiv").fadeIn() //Shows temp labels
     }, 300);
-    setDates()
+    setDates(0)
     scrapeKnolls() //Begins entire process
 }
 
 async function scrapeKnolls() {
-    //let id = "mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com"
-    //let key = "AIzaSyA3Fshq5WSPcvNe8zQTXnbCe6VUArfo13w"
-    var d = new Date()
-    const month = d.getMonth()
-    console.log("month " + month)
+    const month = date.getMonth() + 1 //Without + 1 line 16 returns 400 error
     let site = "https://www.googleapis.com/calendar/v3/calendars/mhrd.org_ccteiaobdj0su75og9mc2u6h4g@group.calendar.google.com/events?key=AIzaSyA3Fshq5WSPcvNe8zQTXnbCe6VUArfo13w&timeMin=2021-" + month + "-01T00:00:00-00:00"
     try {
-        //response = await axios.get(site)
+        response = await axios.get(site)
     } catch (error) { console.log(error) }
     //console.log(response.data)
 }
 
-function setDates() {
-    var d = new Date() //New object
-    //var date = d.getDate()
-    //var month = d.getMonth() + 1 //+1 since JS counts January as 0
-    //console.log("date " + date + " " + month)
-    const month = d.toLocaleString('default', { month: 'long' }).toUpperCase() + " " + d.getFullYear();
-    $("#calSelectHeader > label").text(month) //Sets month
-    var firstDay = new Date(d.getFullYear(), d.getMonth(), 1)
-    firstDay = firstDay.getDay() + 1 //First weekday day of month. i.e. 5 = Friday and is 1st day of month
-    var lastDate = new Date(d.getFullYear(), d.getMonth()+1, 0)
-    lastDate = lastDate.getDate() //Gets final date
-    var dateCount = 1
+function setDates(direction) { //Updates and sets the dates on the calendar during initiation or month change
+    var d = date //Avoids hoisting with correct scope. Thanks https://stackoverflow.com/a/25559623/13297669!. Never mind, removed window... started working on its own? Guess it isn't necessary now...
+    d.setMonth(d.getMonth() + direction) //Sets new month. This is important not to forget!. Wow, this changes the original object?...
+    const monthYear = d.toLocaleString('default', { month: 'long' }).toUpperCase() + " " + d.getFullYear();
+    $("#calSelectHeader > label").text(monthYear) //Sets month header with year
+
+    d.setDate(1) //Sets to 1st of the month
+    var firstDay = d.getDay() + 1 //First weekday integer (1-7) day of month. i.e. 6 = Friday and is 1st day of month
+
+    var lastDate = new Date(d.getFullYear(), d.getMonth() + direction, 0) //Creates object of the last day of the new month. Day 0 of the next month (already set line 23, no +1) does this.
+    lastDate = lastDate.getDate() //Gets final date. i.e. 30 or 31. Starts at 1, not 0, so returned is the actual day
+    var dateCount = 1 //Counter for date to put in calendar slots
     for(var a = firstDay; dateCount <= lastDate; a++) {
         $("#slot" + a).text(dateCount) //Sets current month dates
-        dateCount++
+        $("#slot" + a).css("color", "68C678"); //Forgot to set colors back, lol
+        dateCount++ //Adds 1 to the date to put in calendar slot
     }
-    d.setDate(1); //Going to 1st of the month
-    d.setHours(-1); //Last hour of previous day
-    var lastDayPrevMonth = d.getDate()
+
+    var prevMonth = new Date(d.getFullYear(), d.getMonth(), 0)
+    lastDayPrevMonth = prevMonth.getDate() //Gets last day of previous month
     for(var b = firstDay - 1; b > 0; b--) {
         $("#slot" + b).text(lastDayPrevMonth); //Sets previous month dates
         $("#slot" + b).css("color", "DBDBDB"); //Changes color since not really important...
