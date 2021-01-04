dd59059b10ccdd24aba5bafe0d40b9c7538cea38
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2021-01-04 15:57:03 -0500

    Major JS changes

--- js/knolls.js
+++ js/knolls.js
@@ -6,3 +6,16 @@ var eventDate = []
 
 let month
 let year
+
+function appendKnolls(data) {
+    console.log(data.length)
+    console.log(data)
+
+    for(i = 1; i < data.length; i++) {
+        var eventDiv = document.createElement("div");
+        eventDiv.setAttribute("class", "eventTemplate");
+        eventDiv.appendChild(document.createTextNode(i + ": " + data[i].summary + " " + JSON.stringify(data[i].start)));
+        var element = document.getElementById("eventList");
+        element.appendChild(eventDiv);
+    }
+}
