5d2a8a78b32d1ef4029e107c3571aeca31763f4e
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2021-01-07 17:24:39 -0500

    Fixed months sticking bug

--- js/knolls.js
+++ js/knolls.js
@@ -1,17 +1,20 @@
-var eventName = []
 var eventStatus = [] //Possible: null, Virtual, Canceled
 var eventRoom = []
 var eventStart = []
 var eventDate = []
 
-let month
-let year
+var eventName = [] //The cleaned up event name
+var startDate = [] //Integer date of start time
+
+var isVirtual = [] //Only true if zoom or virtual found in summary
+var isZoom = [] //Only true if zoom found in summary
 
 function appendKnolls(data, currentMonthInt) {
-    console.log(currentMonthInt)
+    //console.log(currentMonthInt)
     var tempDate = new Date(2020, currentMonthInt - 1)
     const month = tempDate.toLocaleString('default', { month: 'short' }).toUpperCase();
-    //$(".eventTemplate").remove(); //Clear previous events, if there are any
+    $(".eventTemplate:not(:first)").remove(); //Clear previous events from DOM, if there are any
+    $(".eventTemplate").eq(0).hide() //Hide the first one
     for(var i = 0; i < data.length; i++) {
         var d
         if (data[i].start.date != undefined) {
@@ -19,12 +22,12 @@ function appendKnolls(data, currentMonthInt) {
         } else {
             d = new Date(data[i].start.dateTime)
         }
-        console.log(d.getMonth() + " hi there")
+        startDate[i] = d.getDate()
         if (d.getMonth() != currentMonthInt - 1) { //Or add one to the former
             d.setDate(1)
         }
-        $(".eventTemplate").eq(0).clone().insertAfter(".eventTemplate:last");
-        $(".pEventName").eq(i + 1).text(data[i].summary)
+        var newElement = $(".eventTemplate").eq(0).clone().show().insertAfter(".eventTemplate:last"); //Can also shoiw after line 15
+        $(".pEventName").eq(i + 1).text(getName(data[i].summary))
         $(".pEventDate").eq(i + 1).text(month + " " + d.getDate())
 
         /*console.log(i)
@@ -34,8 +37,36 @@ function appendKnolls(data, currentMonthInt) {
         var element = document.getElementById("eventList");
         element.appendChild(eventDiv);*/
     }
+    //$(".eventTemplate").eq(0).remove();
+    //$(".eventTemplate").show();
 }
 
+function getName(summary) {
+    summary = checkIfVirtual(summary)
+    return summary
+}
+
+function checkIfVirtual(summary) { //Sees if event is virtual and returns name after cut
+    var isVirtualFront = ["VIRTUAL - "]
+    var isVirtualBack = [" (Zoom)"]
+    for (v in isVirtualFront) {
+        if(summary.includes(isVirtualFront[v])) {
+            //console.log(isVirtualFront[v])
+            splitName = summary.split(isVirtualFront[v])
+            summary = splitName[1]
+            isVirtual[v] = true
+            break
+        }
+        if(summary.includes(isVirtualBack[v])) {
+            splitName = summary.split(isVirtualBack[v])
+            //summary = splitName[0]
+            isVirtual[v] = true
+            isZoom[v] = true
+            break
+        }
+    }
+    return summary
+}
 
 //Left and right arrow keys to change months
 document.onkeydown = checkKey
