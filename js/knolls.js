bc0b24ad5d1dbc2f4625afa8885bbab9f75df94d
JoyousJohn <70230496+JoyousJohn@users.noreply.github.com>
2021-01-09 13:08:57 -0500

    Updated JS

--- js/knolls.js
+++ js/knolls.js
@@ -9,6 +9,10 @@ var startDate = [] //Integer date of start time
 var isVirtual = [] //Only true if zoom or virtual found in summary
 var isZoom = [] //Only true if zoom found in summary
 
+function load() { //Might probably need to use for something later...
+    //$(".pEventVirtual").hide()
+}
+
 function appendKnolls(data, currentMonthInt) {
     //console.log(currentMonthInt)
     var tempDate = new Date(2020, currentMonthInt - 1)
@@ -26,43 +30,40 @@ function appendKnolls(data, currentMonthInt) {
         if (d.getMonth() != currentMonthInt - 1) { //Or add one to the former
             d.setDate(1)
         }
+        analyzeSummary(data[i].summary, i) //Dissects name
         var newElement = $(".eventTemplate").eq(0).clone().show().insertAfter(".eventTemplate:last"); //Can also shoiw after line 15
-        $(".pEventName").eq(i + 1).text(getName(data[i].summary))
+        $(".pEventName").eq(i + 1).text(eventName[i])
         $(".pEventDate").eq(i + 1).text(month + " " + d.getDate())
-
-        /*console.log(i)
-        var eventDiv = document.createElement("div");
-        eventDiv.setAttribute("class", "eventTemplate");
-        eventDiv.appendChild(document.createTextNode(i + ": " + data[i].summary + " " + JSON.stringify(data[i].start))); //Temporary obviously
-        var element = document.getElementById("eventList");
-        element.appendChild(eventDiv);*/
+        if (isVirtual[i] == true) {
+            $(".pEventStatus").eq(i + 1).css("visibility", "visible")
+            $(".pEventStatus").eq(i + 1).text("VIRTUAL")
+            if (isZoom[i] == true) {
+                $(".pEventStatus").eq(i + 1).text("ZOOM")
+            }
+        }
     }
-    //$(".eventTemplate").eq(0).remove();
-    //$(".eventTemplate").show();
 }
 
-function getName(summary) {
-    summary = checkIfVirtual(summary)
-    return summary
+function analyzeSummary(summary, e) {
+    summary = checkIfVirtual(summary, e)
+    eventName[e] = summary
 }
 
-function checkIfVirtual(summary) { //Sees if event is virtual and returns name after cut
-    var isVirtualFront = ["VIRTUAL - "]
+function checkIfVirtual(summary, e) { //Sees if event is virtual and returns name after cut
+    var isVirtualFront = ["VIRTUAL - ", "Virtual "]
     var isVirtualBack = [" (Zoom)"]
     for (v in isVirtualFront) {
         if(summary.includes(isVirtualFront[v])) {
             //console.log(isVirtualFront[v])
             splitName = summary.split(isVirtualFront[v])
             summary = splitName[1]
-            isVirtual[v] = true
-            break
+            isVirtual[e] = true
         }
         if(summary.includes(isVirtualBack[v])) {
             splitName = summary.split(isVirtualBack[v])
-            //summary = splitName[0]
-            isVirtual[v] = true
-            isZoom[v] = true
-            break
+            summary = splitName[0]
+            isVirtual[e] = true
+            isZoom[e] = true
         }
     }
     return summary
